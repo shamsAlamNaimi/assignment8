8a643fe8ef3679a428ae9b42265c229ffab6b9a1
shamsAlamNaimi <naimi.s@husky.neu.edu>
2021-03-31 17:31:21 -0400

    adding build file

---
+++ fe/build/precache-manifest.dfcbc9e573587e8bf8952528949cd2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aac759b5fa4132d07f87715a7af2af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d2e97109711126b0815",
+    "url": "/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "5f1db18059a85a7bcc72",
+    "url": "/static/js/2.7fecb6f8.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.7fecb6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2e97109711126b0815",
+    "url": "/static/js/main.f8c654c2.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
